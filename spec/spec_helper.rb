332d7a595a965c40f931afa05021083f2d2fad89
Nik Davis <nikeadavis@gmail.com>
2014-08-16 22:31:38 -0400

    Add FactoryGirl config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,13 @@
 require 'adroll'
 require 'webmock/rspec'
+require 'factory_girl'
+
+RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+end
+
+FactoryGirl.definition_file_paths = [File.expand_path('../factories', __FILE__)]
+FactoryGirl.find_definitions
 
 ENV['USERNAME'] = 'USERNAME'
 ENV['PASSWORD'] = 'PASSWORD'
