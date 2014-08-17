eb6fb709d11cbaab9d4816bdd0a22b30e6b4353f
Nik Davis <nikeadavis@gmail.com>
2014-08-16 20:50:29 -0400

    Add AdRoll::Api module

--- lib/adroll.rb
+++ lib/adroll.rb
@@ -1,5 +1,10 @@
-require "httparty"
+require 'httparty'
 
 Dir[File.dirname(__FILE__) + '/adroll/*.rb'].each do |file|
-    require file
+  require file
+end
+
+module AdRoll
+  module Api
+  end
 end
