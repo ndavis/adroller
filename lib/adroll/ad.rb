cda9efc62a1b76d37dc396af8f65bda70eb26ebe
Nikolas Davis <nikolasliamdavis@gmail.com>
2015-10-17 18:12:40 -0400

    Add Ad Service methods

--- lib/adroll/ad.rb
+++ lib/adroll/ad.rb
@@ -2,7 +2,63 @@ require 'adroll/service'
 
 module AdRoll
   module Api
-    class Ad < Service
+    class Ad < AdRoll::Api::Service
+      class << self
+        def clone(destination_url: nil, name: nil, headline: nil, body: nil)
+          params = {
+            destination_url: destination_url,
+            name: name,
+            headline: headline,
+            body: body
+          }.reject { |_, value| value.nil? }
+
+          call_api(:post, __method__, params)
+        end
+
+        def create(advertisable:, file:, destination_url: nil, name: nil,
+                   headline: nil, body: nil, message: nil, product: nil, logo: nil)
+          params = {
+            advertisable: advertisable,
+            file: file,
+            destination_url: destination_url,
+            name: name,
+            headline: headline,
+            body: body,
+            product: product,
+            logo: logo
+          }.reject { |_, value| value.nil? }
+
+          call_api(:post, __method__, params)
+        end
+
+        def edit(ad:, destination_url: nil, name: nil, headline: nil, body: nil)
+          params = {
+            ad: ad,
+            destination_url: destination_url,
+            name: name,
+            headline: headline,
+            body: body
+          }.reject { |_, value| value.nil? }
+
+          call_api(:put, __method__, params)
+        end
+
+        def get(ad:)
+          params = {
+            ad: ad
+          }.reject { |_, value| value.nil? }
+
+          call_api(:get, __method__, params)
+        end
+
+        def set_outline(ad:)
+          params = {
+            ad: ad
+          }.reject { |_, value| value.nil? }
+
+          call_api(:get, __method__, params)
+        end
+      end
     end
   end
 end
---
+++ spec/lib/adroll/ad_spec.rb
@@ -0,0 +1,75 @@
+require 'spec_helper'
+
+describe AdRoll::Api::Ad do
+
+  let!(:basic_auth) { "#{AdRoll::Api.user_name}:#{AdRoll::Api.password}" }
+
+  subject { described_class }
+
+  describe '::clone' do
+    let!(:request_uri) { "https://#{basic_auth}@api.adroll.com/v1/ad/clone" }
+
+    let!(:params) do
+      {
+        name: 'name',
+        headline: 'headline',
+        body: 'body'
+      }
+    end
+
+    it 'calls the api with the correct params' do
+      subject.clone(name: 'name', headline: 'headline', body: 'body')
+      expect(WebMock).to have_requested(:post, request_uri).with(query: params)
+    end
+  end
+
+  describe '::create' do
+    let!(:request_uri) { "https://#{basic_auth}@api.adroll.com/v1/ad/create" }
+    let!(:params) do
+      { advertisable: 'advertisable',
+        file: 'ANBLE5432'
+      }
+    end
+
+    it 'calls the api with the correct params' do
+      subject.create(advertisable: 'advertisable', file: 'ANBLE5432')
+      expect(WebMock).to have_requested(:post, request_uri).with(query: params)
+    end
+  end
+
+  describe '::edit' do
+    let!(:request_uri) { "https://#{basic_auth}@api.adroll.com/v1/ad/edit" }
+    let!(:params) do
+      { ad: 'AD123' }
+    end
+
+    it 'calls the api with the correct params' do
+      subject.edit(ad: 'AD123')
+      expect(WebMock).to have_requested(:put, request_uri).with(query: params)
+    end
+  end
+
+  describe '::get' do
+    let!(:request_uri) { "https://#{basic_auth}@api.adroll.com/v1/ad/get" }
+    let!(:params) do
+      { ad: 'AD123' }
+    end
+
+    it 'calls the api with the correct params' do
+      subject.get(ad: 'AD123')
+      expect(WebMock).to have_requested(:get, request_uri).with(query: params)
+    end
+  end
+
+  describe '::set_outline' do
+    let!(:request_uri) { "https://#{basic_auth}@api.adroll.com/v1/ad/set_outline" }
+    let!(:params) do
+      { ad: 'AD123' }
+    end
+
+    it 'calls the api with the correct params' do
+      subject.set_outline(ad: 'AD123')
+      expect(WebMock).to have_requested(:get, request_uri).with(query: params)
+    end
+  end
+end
