78df40245da5b237bfc51fbb086a8ed21985ba12
Nikolas Davis <nikolasliamdavis@gmail.com>
2015-11-07 15:09:28 -0500

    Add Pixel Service

--- lib/adroll/pixel.rb
+++ lib/adroll/pixel.rb
@@ -1,8 +1,45 @@
-require 'adroll/service'
-
 module AdRoll
   module Api
-    class Pixel < Service
+    class Pixel < AdRoll::Api::Service
+      class << self
+        def get(pixel: )
+          params = {
+            pixel: pixel
+          }.reject { |_, value| value.nil? }
+
+          call_api(:get, __method__, params)
+
+
+        end
+
+        def get_rules(pixel: )
+          params = {
+            pixel: pixel
+          }.reject { |_, value| value.nil? }
+
+          call_api(:get, __method__, params)
+
+        end
+
+        def get_segments(pixel: )
+          params = {
+            pixel: pixel
+          }.reject { |_, value| value.nil? }
+
+          call_api(:get, __method__, params)
+
+        end
+
+        def reorder_rules(pixel: , rules: )
+          params = {
+            pixel: pixel,
+            rules: rules
+          }.reject { |_, value| value.nil? }
+
+          call_api(:get, __method__, params)
+
+        end
+      end
     end
   end
 end
