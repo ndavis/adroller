a1299b3337b8666ac358b8334c48d5c4796e1e81
Nikolas Davis <nikolasliamdavis@gmail.com>
2015-11-07 19:51:15 -0500

    Add Rule Service

--- lib/adroll/rule.rb
+++ lib/adroll/rule.rb
@@ -1,8 +1,50 @@
-require 'adroll/service'
-
 module AdRoll
   module Api
-    class Rule < Service
+    class Rule < AdRoll::Api::Service
+      class << self
+        def create(pixel: , type: , order: , name: , display_name: , pattern: , source: )
+          params = {
+            pixel: pixel,
+            type: type ,
+            order: order ,
+            name: name,
+            display_name: display_name ,
+            pattern: pattern,
+            source: source,
+          }.reject { |_, value| value.nil? }
+
+          call_api(:post, __method__, params)
+        end
+
+        def edit(rule: , order: , name: , display_name: , pattern: , source: )
+          params = {
+            rule: rule,
+            order: order ,
+            name: name,
+            display_name: display_name ,
+            pattern: pattern,
+            source: source,
+          }.reject { |_, value| value.nil? }
+
+          call_api(:put, __method__, params)
+        end
+
+        def get(rule: )
+          params = {
+            rule: rule
+          }.reject { |_, value| value.nil? }
+
+          call_api(:get, __method__, params)
+        end
+
+        def get_segments(rule: )
+          params = {
+            rule: rule
+          }.reject { |_, value| value.nil? }
+
+          call_api(:get, __method__, params)
+        end
+      end
     end
   end
 end
