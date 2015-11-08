34260e4478dadc9db3c9072cf660ef849a3365e5
Nikolas Davis <nikolasliamdavis@gmail.com>
2015-11-07 19:51:45 -0500

    Add User Service

--- lib/adroll/user.rb
+++ lib/adroll/user.rb
@@ -1,8 +1,49 @@
-require 'adroll/service'
-
 module AdRoll
   module Api
-    class User < Service
+    class User < AdRoll::Api::Service
+      class << self
+        def deactivate(user: )
+          params = {
+            user: user
+          }.reject { |_, value| value.nil? }
+
+          call_api(:get, __method__, params)
+        end
+
+        def edit(u: , username:, first_name: , last_name: , email_preference_general: ,
+                 email_preference_payment: , email_preference_campaign_notifications: )
+          params = {
+            u: u ,
+            username: username,
+            first_name: first_name,
+            last_name: last_name,
+            email_preference_general: email_preference_general,
+            email_preference_payment: email_preference_payment,
+            email_preference_campaign_notifications: email_preference_campaign_notifications
+          }.reject { |_, value| value.nil? }
+
+          call_api(:put, __method__, params)
+        end
+
+        def get(user: )
+          params = {
+            user: user
+          }.reject { |_, value| value.nil? }
+
+          call_api(:get, __method__, params)
+        end
+
+        def grant(u: , advertisables: , organization_role: )
+
+          params = {
+            u: u,
+            advertisables: advertisables,
+            organization_role: organization_role
+          }.reject { |_, value| value.nil? }
+
+          call_api(:get, __method__, params)
+        end
+      end
     end
   end
 end
