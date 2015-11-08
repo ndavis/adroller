d2be5ad018af8909c45af7f3d007f991e1c991b9
Nikolas Davis <nikolasliamdavis@gmail.com>
2015-11-07 19:51:29 -0500

    Add Segment Service

--- lib/adroll/segment.rb
+++ lib/adroll/segment.rb
@@ -1,8 +1,24 @@
-require 'adroll/service'
-
 module AdRoll
   module Api
-    class Segment < Service
+    class Segment < AdRoll::Api::Service
+      class << self
+        def edit(conversion_value: , duration: )
+          params = {
+            conversion_value: conversion_value,
+            duration: duration
+          }.reject { |_, value| value.nil? }
+
+          call_api(:post, __method__, params)
+        end
+
+        def get(segment: )
+          params = {
+            segment: segment,
+          }.reject { |_, value| value.nil? }
+
+          call_api(:get, __method__, params)
+        end
+      end
     end
   end
 end
