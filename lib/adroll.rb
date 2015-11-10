4168901a536215db2fa434fcd3ee0ed03412af7c
Nikolas Davis <nikolasliamdavis@gmail.com>
2015-11-10 15:54:14 -0500

    Remove File.dirname

--- lib/adroll.rb
+++ lib/adroll.rb
@@ -1,8 +1,21 @@
 require 'httparty'
 
-Dir[File.dirname(__FILE__) + '/adroll/*.rb'].each do |file|
-  require file
-end
+require 'adroll/ad'
+require 'adroll/adgroup'
+require 'adroll/advertisable'
+require 'adroll/campaign'
+require 'adroll/event'
+require 'adroll/invoice'
+require 'adroll/mobile_app'
+require 'adroll/mobile_rule'
+require 'adroll/organization'
+require 'adroll/payment_method'
+require 'adroll/pixel'
+require 'adroll/report'
+require 'adroll/rule'
+require 'adroll/segment'
+require 'adroll/service'
+require 'adroll/user'
 
 module AdRoll
   module Api
